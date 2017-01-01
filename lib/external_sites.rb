1db9e8f35a92e23dcbef62652ddee062fcb2c259
Poojan Shrestha <poojans@gmail.com>
2017-01-01 22:37:29 +0545

    Added a module for extracting tag data from external sites.

---
+++ lib/external_sites.rb
@@ -0,0 +1,22 @@
+module ExternalSites
+  def self.fetch(url)
+    Nokogiri::HTML(open(url)) do |config|
+      config.options = Nokogiri::XML::ParseOptions::NOBLANKS
+    end
+  end
+
+  def self.extract_tags_data(url)
+    doc = self.fetch(url)
+
+    tags = ['h1', 'h2', 'h3', 'a']
+    tags.reduce([]) do |json, tag|
+      doc.search(tag).each do |el|
+        unless el.text.strip == ''
+          json.push({ tag: tag, body: el.text.strip })
+        end
+      end
+      json
+    end
+  end
+
+end
---
+++ test/lib/external_sites_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+require 'external_sites'
+
+describe ExternalSites do
+  let(:url) { 'http://www.example1.com' }
+
+  before do
+    stub_request(:get, url)
+      .with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'})
+      .to_return(:status => 200, :body => "<h1>heading 1</h1><h2>heading 2</h2>", :headers => {})
+  end
+
+  it "should extract tags" do
+    tags_data = ExternalSites.extract_tags_data(url)
+    assert_equal tags_data.length, 2, "No of tags do not match."
+    assert_requested(:get, url, times: 1)
+  end
+
+end
