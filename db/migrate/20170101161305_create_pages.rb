73f1eaffa1a4f8a749b27111246037016b95dd65
Poojan Shrestha <poojans@gmail.com>
2017-01-01 21:58:23 +0545

    Generate scaffold for page.

---
+++ db/migrate/20170101161305_create_pages.rb
@@ -0,0 +1,10 @@
+class CreatePages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pages do |t|
+      t.string :url
+
+      t.timestamps
+    end
+    add_index :pages, :url, unique: true
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  let(:page) { pages :one }
+
+  it "gets index" do
+    get pages_url
+    value(response).must_be :success?
+  end
+
+  it "creates page" do
+    expect {
+      post pages_url, params: { page: { url: page.url } }
+    }.must_change "Page.count"
+
+    value(response.status).must_equal 201
+  end
+
+  it "shows page" do
+    get page_url(page)
+    value(response).must_be :success?
+  end
+
+  it "updates page" do
+    patch page_url(page), params: { page: { url: page.url } }
+    value(response.status).must_equal 200
+  end
+
+  it "destroys page" do
+    expect {
+      delete page_url(page)
+    }.must_change "Page.count", -1
+
+    value(response.status).must_equal 204
+  end
+end
